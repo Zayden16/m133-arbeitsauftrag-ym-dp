619bce907a5afd43321ead40537d79a07cefcbb7
Yazdan Musa <yazdanmusa@gmail.com>
2020-11-24 16:46:24 +0100

    drag n drop functionality, separated particles.js

--- frontend/app.js
+++ frontend/app.js
@@ -1,3 +1,45 @@
-const particlesJSON={"particles":{"number":{"value":160,"density":{"enable":true,"value_area":800}},"color":{"value":"#cc6633"},"shape":{"type":"circle","stroke":{"width":0,"color":"#000000"},"polygon":{"nb_sides":3},"image":{"src":"img/github.svg","width":100,"height":100}},"opacity":{"value":1,"random":true,"anim":{"enable":true,"speed":1,"opacity_min":0,"sync":false}},"size":{"value":3.945738208161363,"random":true,"anim":{"enable":false,"speed":4,"size_min":0.3,"sync":false}},"line_linked":{"enable":true,"distance":100,"color":"#ffffff","opacity":0.5,"width":0.5},"move":{"enable":true,"speed":1,"direction":"none","random":true,"straight":false,"out_mode":"out","bounce":false,"attract":{"enable":false,"rotateX":600,"rotateY":600}}},"interactivity":{"detect_on":"canvas","events":{"onhover":{"enable":false,"mode":"bubble"},"onclick":{"enable":false,"mode":"repulse"},"resize":true},"modes":{"grab":{"distance":400,"line_linked":{"opacity":1}},"bubble":{"distance":250,"size":0,"duration":2,"opacity":0,"speed":3},"repulse":{"distance":400,"duration":0.4},"push":{"particles_nb":4},"remove":{"particles_nb":2}}},"retina_detect":true};
+const listItems = document.querySelectorAll('.card');
+const lists = document.querySelectorAll('.card-list');
 
-particlesJS("particles-js", particlesJSON)
+let draggedItem = null;
+
+for (let i = 0; i < listItems.length; i++) {
+    const item = listItems[i];
+
+    item.addEventListener('dragstart', function () {
+        draggedItem = item;
+        setTimeout(function () {
+            item.style.display = 'none';
+        }, 0)
+    });
+
+    item.addEventListener('dragend', function () {
+        setTimeout(function () {
+            draggedItem.style.display = 'block';
+            draggedItem = null;
+        }, 0);
+    })
+
+    for (let j = 0; j < lists.length; j++) {
+        const list = lists[j];
+
+        list.addEventListener('dragover', function (e) {
+            e.preventDefault();
+        });
+
+        list.addEventListener('dragenter', function (e) {
+            e.preventDefault();
+            this.style.backgroundColor = 'rgba(0, 0, 0, 0.2)';
+        });
+
+        list.addEventListener('dragleave', function (e) {
+            this.style.backgroundColor = 'rgba(0, 0, 0, 0.1)';
+        });
+
+        list.addEventListener('drop', function (e) {
+            console.log('drop');
+            this.append(draggedItem);
+            this.style.backgroundColor = 'rgba(0, 0, 0, 0.1)';
+        });
+    }
+}
