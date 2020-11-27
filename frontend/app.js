917aeb4abda7cfb9466fe5910e8cb65b57d369a4
hammerhc <hammerhc@users.noreply.github.com>
2020-11-27 08:36:41 +0100

    Added loadcards functionality
    Deleted cards in app.html
    Added load event / function
    Added loadCard function to get cards from API
    Added createCards funtion to add the cards to the html

--- frontend/app.js
+++ frontend/app.js
@@ -1,46 +1,101 @@
-const listItems = document.querySelectorAll('.card');
-const lists = document.querySelectorAll('.card-list');
-
-let draggedItem = null;
-
-for (let i = 0; i < listItems.length; i++) {
-    const item = listItems[i];
-
-    item.addEventListener('dragstart', function () {
-        draggedItem = item;
-        setTimeout(function () {
-            item.style.display = 'none';
-        }, 0)
+function load() {   
+    getCards( function (isValid, response) {
+        if (isValid) {
+            createCards(response);
+            addDragEvents();
+        }
     });
+}
 
-    item.addEventListener('dragend', function () {
-        setTimeout(function () {
-            draggedItem.style.display = 'block';
-            draggedItem = null;
-        }, 0);
-    })
+function addDragEvents() {
+    const listItems = document.querySelectorAll('.card');
+    const lists = document.querySelectorAll('.card-list');
 
-    for (let j = 0; j < lists.length; j++) {
-        const list = lists[j];
+    let draggedItem = null;
 
-        list.addEventListener('dragover', function (e) {
-            e.preventDefault();
-            this.style.backgroundColor = '#cc6633';
+    for (let i = 0; i < listItems.length; i++) {
+        const item = listItems[i];
+
+        item.addEventListener('dragstart', function () {
+            draggedItem = item;
+            setTimeout(function () {
+                item.style.display = 'none';
+            }, 0)
         });
 
-        list.addEventListener('dragenter', function (e) {
-            e.preventDefault();
-            this.style.backgroundColor = '#323234';
-        });
+        item.addEventListener('dragend', function () {
+            setTimeout(function () {
+                draggedItem.style.display = 'block';
+                draggedItem = null;
+            }, 0);
+        })
 
-        list.addEventListener('dragleave', function (e) {
-            this.style.backgroundColor = '#212121';
-        });
+        for (let j = 0; j < lists.length; j++) {
+            const list = lists[j];
 
-        list.addEventListener('drop', function (e) {
-            console.log('drop');
-            this.append(draggedItem);
-            this.style.backgroundColor = 'rgba(0, 0, 0, 0.1)';
+            list.addEventListener('dragover', function (e) {
+                e.preventDefault();
+                this.style.backgroundColor = '#cc6633';
+            });
+
+            list.addEventListener('dragenter', function (e) {
+                e.preventDefault();
+                this.style.backgroundColor = '#323234';
+            });
+
+            list.addEventListener('dragleave', function (e) {
+                this.style.backgroundColor = '#212121';
+            });
+
+            list.addEventListener('drop', function (e) {
+                console.log('drop');
+                this.append(draggedItem);
+                this.style.backgroundColor = 'rgba(0, 0, 0, 0.1)';
+            });
+        }
+    }
+}
+
+function getCards(callback) {
+    fetch('/api/LoadCards')
+        .then(
+            function (response) {
+                response.json().then(function (data) {
+                    callback(true, data);
+                });
+            }
+        )
+        .catch(function (err) {
+            callback(false, err);
+            console.log('Fetch Error :-S', err);
         });
+}
+
+function createCards(cards) {
+    var todo = document.querySelector("#todo");
+    var inProgress = document.querySelector("#in-progess");
+    var done = document.querySelector("#done");
+
+    todo.innerHTML = "<h1>To-Do</h1>";
+    inProgress.innerHTML = "<h1>In-Progress</h1>";
+    done.innerHTML = "<h1>Done</h1>";
+
+    for (var i = 0; i < cards.length; i++) {
+        var card = `<article class="card" draggable="true">
+                        <header class="card-header">
+                            <h2>${cards[i].title}</h2>
+                            <p>${cards[i].text}</p>
+                        </header>
+                    </article>`;
+
+        if (cards[i].position == 1) { // In Progress
+            inProgress.innerHTML += card;
+          
+        } else if (cards[i].position == 2) { // Done  
+            done.innerHTML += card;
+        
+        } else { // To Do
+            todo.innerHTML += card;
+        }
     }
 }
